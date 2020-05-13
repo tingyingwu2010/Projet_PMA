d84f0e104d7237983aa72a8a13a41015faa2b504
mac <mac@macdeMBP.mshome.net>
2020-05-13 20:55:33 +0200

    heristic

---
+++ Heuristic.jl
@@ -0,0 +1,35 @@
+function Initialisation(capacity, numitem, weight)
+    # A = Array{Array}(undef, 9)
+    numitem = trunc(Int,numitem)
+    # number of items
+    i = 1
+    # number of bins
+    bin = 0
+    # Used capacity in bin
+    cap_used = zeros(1, numitem)
+    # Packed in bin
+    bin_item = Array{Array}(undef, numitem)
+    for j in 1 : numitem
+        bin_item[j] = zeros(1, numitem)
+    end
+    # There are still items to be packed
+    while (i <= numitem)
+        for j in 1 : numitem
+            if(cap_used[j] + weight[i] < 1)
+                cap_used[j] = cap_used[j] + weight[i]
+                bin_item[j][i] = 1
+                i = i + 1
+                break
+            end
+        end
+    end
+    for j in 1 : numitem
+        if(sum(bin_item[j])==0.0)
+            bin = j - 1
+            break
+        end
+    end
+    bin_item = bin_item[1:bin]
+
+    return bin, bin_item
+end
--- READdata.jl
+++ READdata.jl
@@ -6,9 +6,10 @@ function ReadDate(path)
 
     Falkenauer = readdlm(path)
     capacity = Falkenauer[2]
-    numbin = Falkenauer[1]
+    numitem = Falkenauer[1]
     weight = Falkenauer[3:122]
-
-    return capacity,numbin,weight
+    weight = weight/capacity
+    weight = sort(weight)
+    return capacity,numitem,weight
 
 end
