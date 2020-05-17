8ae971c41eb9694f727861e4c592d2601e51b191
mac <mac@macdeMBP.mshome.net>
2020-05-17 16:31:24 +0200

    commander

--- READdata.jl
+++ READdata.jl
@@ -5,11 +5,17 @@ using DelimitedFiles
 function ReadDate(path)
 
     Falkenauer = readdlm(path)
+    # the capacity of each bin
     capacity = Falkenauer[2]
+    # the number of item
     numitem = Falkenauer[1]
+    # the weight of each item
     weight = Falkenauer[3:length(Falkenauer)]
+    # normalize the weight
     weight = weight/capacity
+    # sort the weight array
     weight = sort(weight)
+    
     return capacity,numitem,weight
 
 end
--- result.jl
+++ result.jl
@@ -1,11 +1,13 @@
 function result_processing(result_pattern, result_index, numitem, pattern_bin)
     # result_pattern: the result of each bin after branch and price
 
+    # reshape the result_pattern to make it possible to insert index.
     for k in 1:length(result_pattern)
         result_pattern[k] = floor.(Int,result_pattern[k])
         result_pattern[k] = reshape(result_pattern[k], (numitem,))
     end
 
+    # insert the items already packed by the initialization
     for i in sort(result_index)
         for k in 1:length(result_pattern)
             insert!(result_pattern[k], i, 0)
@@ -13,6 +15,6 @@ function result_processing(result_pattern, result_index, numitem, pattern_bin)
     end
 
 
-
+    # return the final result
     return vcat(pattern_bin,result_pattern)
 end
