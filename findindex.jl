52b4035d9d92efb5ad493bfa1d04da650db6258c
mac <mac@macdeMBP.mshome.net>
2020-05-17 16:47:19 +0200

    comm

--- findindex.jl
+++ findindex.jl
@@ -1,5 +1,5 @@
 function findindex(A, num)
-    # find the index in array A correspond to num
+    # find the index in array A correspond to number
     index = []
     for j in 1:length(A)
         if(A[j] == num)
